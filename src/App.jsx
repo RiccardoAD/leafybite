13d36ba5e0caced6bffc01cb100593c64a7aaa81
RiccardoAD <rick_95@libero.it>
2024-10-24 23:25:02 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+
+import {
+  
+  // Route
+  
+   Routes, BrowserRouter } from "react-router-dom";
+// import Home from './pages/Home';
+// import Searched from './pages/Searched/Searched';
+// import Recipe from './pages/Recipe/Recipe';
+import Navbar from '../src/components/Navbar/Navbar.jsx';
+import Footer from '../src/components/Footer/Footer.jsx';
+
+// import Favorites from './pages/Favorites/Favorites';
+import './App.css';
+
+// import { FavoritesProvider } from './FavoritesContext';
+
+function App() {
+
+  return (
+    // <FavoritesProvider>
+        <BrowserRouter
+        //  basename={process.env.PUBLIC_URL}
+         >
+        <Navbar />
+          <Routes>
+            {/* <Route exact path="/" element={<Home />} />
+            <Route path="/searched/:search" element={<Searched />} />
+            <Route path="/recipe/:name" element={<Recipe />} /> */}
+            {/* <Route path="/favorites/" element={<Favorites />} /> */}
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+    // </FavoritesProvider>
+  );
+}
+
+export default App;
